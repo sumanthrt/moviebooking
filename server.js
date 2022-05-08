b434cecc5a5438d94ec63724322ae7e17b4f9a8d
Sumanth Tellakula <56023590+sumanthrt@users.noreply.github.com>
2022-05-08 19:08:30 +0530

    Checkpoint 6 Part 1 Complete

--- server.js
+++ server.js
@@ -1,45 +1,63 @@
-// configure port details and import required modules
+// // configure port details and import required modules
+// const port = 9000;
+// const http = require("http");
+// const httpStatus = require("http-status-codes");
+// const db = require("./models");
+
+// // creating a route response map
+// const routeResponseMap = {
+//   "/movies": "All Movies Data in JSON format from Mongo DB",
+//   "/genres": "All Genres Data in JSON format from Mongo DB",
+//   "/artists": "All Artists Data in JSON format from Mongo DB",
+// };
+
+// // create server
+// const app = http.createServer((req, res) => {
+//   let url = req.url;
+//   if (routeResponseMap[url]) {
+//     res.writeHead(httpStatus.StatusCodes.OK, {
+//       "Content-Type": "text/html",
+//     });
+//     res.write(routeResponseMap[url]);
+//     res.end();
+//   } else {
+//     res.writeHead(httpStatus.StatusCodes.NOT_FOUND, {
+//       "Content-Type": "text/html",
+//     });
+//     res.write("Error: Page Not Found");
+//     res.end();
+//   }
+// });
+
+// app.listen(port);
+
+// db.mongoose
+//   .connect(db.url, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to the database!");
+//   })
+//   .catch((err) => {
+//     console.log("Cannot connect to the database!", err);
+//     process.exit();
+//   });
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
 const port = 9000;
-const http = require("http");
-const httpStatus = require("http-status-codes");
-const db = require("./models/index");
 
-// creating a route response map
-const routeResponseMap = {
-  "/movies": "All Movies Data in JSON format from Mongo DB",
-  "/genres": "All Genres Data in JSON format from Mongo DB",
-  "/artists": "All Artists Data in JSON format from Mongo DB",
-};
+app.use(cors());
 
-// create server
-const app = http.createServer((req, res) => {
-  let url = req.url;
-  if (routeResponseMap[url]) {
-    res.writeHead(httpStatus.StatusCodes.OK, {
-      "Content-Type": "text/html",
-    });
-    res.write(routeResponseMap[url]);
-    res.end();
-  } else {
-    res.writeHead(httpStatus.StatusCodes.NOT_FOUND, {
-      "Content-Type": "text/html",
-    });
-    res.write("Error: Page Not Found");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to Upgrad Movie booking application development.",
+  });
 });
 
-app.listen(port);
-
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-  })
-  .catch((err) => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+app.listen(port, () => {
+    console.log('Connection Established on PORT ',port);
+});
