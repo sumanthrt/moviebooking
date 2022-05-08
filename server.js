e676d8010983e3bbd2dc1f0ef037647da3ad46c8
Sumanth Tellakula <56023590+sumanthrt@users.noreply.github.com>
2022-05-08 16:13:10 +0530

    Checkpoint 5 Complete

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const port = 9000;
 const http = require("http");
 const httpStatus = require("http-status-codes");
+const db = require("./models/index");
 
 // creating a route response map
 const routeResponseMap = {
@@ -29,3 +30,16 @@ const app = http.createServer((req, res) => {
 });
 
 app.listen(port);
+
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
